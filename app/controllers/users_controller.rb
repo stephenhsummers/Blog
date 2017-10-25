58816e031046ecf7ce73a1356ef27c2d9ffb17e3
stephenhsummers <stephen.summers1@btinternet.com>
2017-10-25 04:33:54 +0100

    Added again

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def index
+    @users =User.all 
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
